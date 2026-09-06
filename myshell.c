3267ee77328366e78e5959729229f28097507de4
xoxoedg <rugyasin@gmail.com>
2026-09-06 12:52:27 +0200

    Support cd

--- myshell.c
+++ myshell.c
@@ -4,27 +4,51 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+void parse_input(char *input, char *args[]) {
+    int i = 0;
+    char *token = strtok(input, " ");
+    while(token != NULL) {
+        args[i] = token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+    
+}
+
+
 int main() {
     char input[256];
+    char *args[10];
+
     while (1) {
         printf("myshell> ");
         if (fgets(input, sizeof(input), stdin) == NULL) {
             break;
         }
         // Ersetze \n mit \0
-        
         input[strcspn(input, "\n")] = '\0';
+        parse_input(input, args);
 
 
         if (strcmp(input, "exit") == 0) {
             break;
         }
+
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "cd: fehlender Pfad\n");
+            } else {
+                chdir(args[1]);
+                continue;
+            }
+            
+        }
         
         pid_t pid = fork();
 
         if (pid == 0) {
-            char *args[] = {"ls", NULL};
-            execvp("ls", args);
+            execvp(args[0], args);
             perror("execvp fehlgeschlagen");   
             exit(1);                        
         } else {
