37c31368c92be9fdc3fc1dfdf6ed279e5b47d989
xoxoedg <rugyasin@gmail.com>
2026-09-09 21:47:07 +0200

    Preparation for Pipelining support

--- myshell.c
+++ myshell.c
@@ -3,6 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+// Return 0 isPipe False
+// Return 1 isPipe True
+int split_pipe_to_buffers(char *input, char *cmd1, char *cmd2) {
+    char* pos = strstr(input, "|");
+
+    if (pos == NULL) {
+        strcpy(cmd1, input);
+        return 0;
+    }
+
+    size_t len1 = pos - input;
+    //Kopiere von Anfang des Strings bis len1 Symbole also bis | beginnt
+    strncpy(cmd1, input, len1);
+    
+    cmd1[len1] = '\0';
+    // +1 Skip Pipe 
+    strcpy(cmd2, pos+1);
+    return 1
+
+}
 
 char* find_and_split(char *input, int *isAnd) {
     // Gibt Startpointer von Substring zurück 
@@ -72,7 +92,18 @@ int handle_builtin(char *args[]) {
         
 }
 
-int run_command(char* args[]) {
+int run_single_command(char* input) {
+    char *args[10];
+    parse_input(input, args);
+
+       if (args[0] == NULL) {
+            return 0;
+        }
+
+        if (handle_builtin(args) == 1) {
+            return 0;
+        }
+
     pid_t pid = fork();
     if (pid == 0) {
         execvp(args[0], args);
@@ -89,9 +120,48 @@ int run_command(char* args[]) {
     return -1;
 } 
 
+int execute_commands(char *cmd_str) {
+    char cmd1_buf[256];
+    char cmd2_buf[256];
+
+    int is_pipe = split_pipe_to_buffers(cmd_str, cmd1_buf, cmd2_buf);
+
+    if(is_pipe == 0) {
+        return run_pipeline(cmd1_buf, cmd2_buf);
+    } else {
+        return run_single_command(cmd1_buf);
+    }
+}
+
+int run_pipeline(char* cmd1_buf, char* cmd2_buf) {
+    int pipefd[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe fehlgeschlagen");
+        return -1;
+    }
+
+    // --------------------------------------------------
+    // KIND 1: Führt den ersten Befehl aus (schreibt in Pipe)
+    // --------------------------------------------------
+    pid1 = fork()
+
+    if (pid1==0) {
+        // Lese Ende schließen
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+    }
+        
+    }
+    
+    
+}
+
+
 int main() {
     char input[256];
-    char *args[10];
     int isAnd;
 
     while (1) {
@@ -101,23 +171,14 @@ int main() {
             break;
         }
         char* second_command = find_and_split(input, &isAnd);
-        parse_input(input, args);
         
-        if (args[0] == NULL) {
-            continue;
-        }
 
-        if (handle_builtin(args) == 1) {
-            continue;
-        }
-        
-        int exit_code = run_command(args);
+        int exit_code = execute_commands(input);
 
         if (second_command != NULL) {
             int is_success = (exit_code == 0);
                if ((isAnd && is_success) || (!isAnd && !is_success)) {
-                    parse_input(second_command, args);
-                    run_command(args); 
+                   execute_command(second_command); 
                 }
             }
         }
